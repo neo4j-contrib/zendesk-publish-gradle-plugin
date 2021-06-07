cf06263f5322fbb8b61d0bdb56f674e6cd058d23
Guillaume Grossetie <ggrossetie@gmail.com>
2021-06-07 18:37:21 +0200

    Set comments_disabled and promoted

--- src/test/kotlin/com/neo4j/gradle/zendesk/ArticleAttributesReaderTest.kt
+++ src/test/kotlin/com/neo4j/gradle/zendesk/ArticleAttributesReaderTest.kt
@@ -4,7 +4,6 @@ package com.neo4j.gradle.zendesk
 import org.assertj.core.api.Assertions.assertThat
 import org.gradle.testfixtures.ProjectBuilder
 import kotlin.test.Test
-import kotlin.test.assertNotNull
 
 class ArticleAttributesReaderTest {
   @Test
@@ -25,7 +24,7 @@ class ArticleAttributesReaderTest {
       "slug" to "cluster-election-rules-and-behavior-explained",
       "private" to "",
       "title" to "Cluster Election Rules and Behavior-Explained"
-      )
+    )
     val articleAttributes = articleAttributesReader.fromMap(attributes, "<h1>Title</h1>", "", "")
     assertThat(articleAttributes).isNotNull
     assertThat(articleAttributes?.slug).isEqualTo("cluster-election-rules-and-behavior-explained")
@@ -89,5 +88,56 @@ class ArticleAttributesReaderTest {
     assertThat(articleAttributes?.author?.name).isEqualTo("Dave Gordon")
     assertThat(articleAttributes?.author?.firstName).isEqualTo("Dave")
     assertThat(articleAttributes?.author?.lastName).isNull()
+  }
+
+  @Test
+  fun `resolve promoted as boolean`() {
+    // Create a test project and apply the plugin
+    val project = ProjectBuilder.builder().build()
+    project.plugins.apply("com.neo4j.gradle.zendesk.ZenDeskPlugin")
+
+    val articleAttributesReader = ArticleAttributesReader(project.logger)
+    val attributes = mapOf(
+      "tags" to listOf("neo4jstaff"),
+      "slug" to "cluster-election-rules-and-behavior-explained",
+      "private" to "",
+      "title" to "Cluster Election Rules and Behavior-Explained",
+      "promoted" to "true",
+      "comments_disabled" to "true"
+    )
+    val articleAttributes = articleAttributesReader.fromMap(attributes, "<h1>Title</h1>", "", "")
+    assertThat(articleAttributes).isNotNull
+    assertThat(articleAttributes?.slug).isEqualTo("cluster-election-rules-and-behavior-explained")
+    assertThat(articleAttributes?.tags).containsExactly("neo4jstaff")
+    assertThat(articleAttributes?.commentsDisabled).isTrue
+    assertThat(articleAttributes?.promoted).isTrue
+  }
+
+  @Test
+  fun `use default comments disabled value`() {
+    // Create a test project and apply the plugin
+    val project = ProjectBuilder.builder().build()
+    project.plugins.apply("com.neo4j.gradle.zendesk.ZenDeskPlugin")
+
+    val articleAttributesReader = ArticleAttributesReader(project.logger)
+    val attributes = mapOf(
+      "tags" to listOf("neo4jstaff"),
+      "slug" to "cluster-election-rules-and-behavior-explained",
+      "private" to "",
+      "title" to "Cluster Election Rules and Behavior-Explained",
+      "promoted" to "true"
+    )
+    val articleAttributes = articleAttributesReader.fromMap(
+      attributes = attributes,
+      content = "<h1>Title</h1>",
+      yamlFileAbsolutePath = "",
+      fileName = "",
+      commentsDisabledDefaultValue = true
+    )
+    assertThat(articleAttributes).isNotNull
+    assertThat(articleAttributes?.slug).isEqualTo("cluster-election-rules-and-behavior-explained")
+    assertThat(articleAttributes?.tags).containsExactly("neo4jstaff")
+    assertThat(articleAttributes?.commentsDisabled).isTrue
+    assertThat(articleAttributes?.promoted).isTrue
   }
 }
